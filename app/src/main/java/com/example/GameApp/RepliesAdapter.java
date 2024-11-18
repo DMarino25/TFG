5db2e9e364dee8135649a4358ba18002facff453
AeksP <alex.pascual.batet@estudiantat.upc.edu>
2024-11-18 23:36:12 +0100

    Replies reports

--- app/src/main/java/com/example/GameApp/RepliesAdapter.java
+++ app/src/main/java/com/example/GameApp/RepliesAdapter.java
@@ -1,19 +1,30 @@
 package com.example.GameApp;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.GameApp.ClassObjectes.Comment;
 import com.example.GameApp.ClassObjectes.Reply;
 import com.example.GameApp.R;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RepliesAdapter extends RecyclerView.Adapter<RepliesAdapter.ReplyViewHolder> {
 
@@ -41,6 +52,12 @@ public class RepliesAdapter extends RecyclerView.Adapter<RepliesAdapter.ReplyVie
                 .load(reply.getReplyUserPicture())
                 .circleCrop()
                 .into(holder.replyUserImageView);
+
+        // Mostrar el PopupMenu al mantener pulsado la respuesta
+        holder.itemView.setOnLongClickListener(v -> {
+            holder.showPopupMenu(holder.itemView, reply, holder.itemView.getContext());
+            return true;
+        });
     }
 
     @Override
@@ -59,5 +76,47 @@ public class RepliesAdapter extends RecyclerView.Adapter<RepliesAdapter.ReplyVie
             replyUserNameTextView = itemView.findViewById(R.id.replyUserNameTextView);
             replyTextView = itemView.findViewById(R.id.replyTextView);
         }
+
+        private void showPopupMenu(View view, Reply reply, Context context) {
+            // Obtengo el  desde el layout del comentario
+            TextView replyTextView = view.findViewById(R.id.replyTextView);
+
+            // Creo el PopupMenu anclado al TextView "Responder"
+            PopupMenu popupMenu = new PopupMenu(context, replyTextView);
+            popupMenu.inflate(R.menu.comment_menu);
+            popupMenu.setForceShowIcon(true);   //Icon
+            // Acciones al hacer click en los elementos del menú
+            popupMenu.setOnMenuItemClickListener(item -> {
+                if (item.getItemId() == R.id.report_comment) {
+                    reportComment(reply);
+                    Toast.makeText(context, "Respuesta reportada", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            });
+
+            // Mostrar el PopupMenu
+            popupMenu.show();
+        }
+
+        private void reportComment(Reply reply) {
+            // Lógica para reportar la respuesta
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            Map<String, Object> report = new HashMap<>();
+            report.put("replyId", reply.getId());                                           //Comment reported
+            report.put("reporterId", FirebaseAuth.getInstance().getCurrentUser().getUid());     //Who reported
+            report.put("reportDate", new Timestamp(new Date()));                                //Date
+
+            db.collection("reports")
+                    .add(report)
+                    .addOnSuccessListener(documentReference ->
+                            Log.d("CommentsAdapter", "Comentario reportado con ID: " + documentReference.getId())
+                    )
+                    .addOnFailureListener(e ->
+                            Log.e("CommentsAdapter", "Error al reportar comentario", e)
+                    );
+        }
+
     }
+
 }
